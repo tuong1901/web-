8a56c2f70d2dd211585120cf488a2df0d14ba7c3
tuong1901 <hoanghoangcattuong@gmail.com>
2025-02-13 15:58:44 +0700

    Add files via upload

---
+++ app1/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using app1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace app1.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly ILogger<HomeController> _logger;
+
+        /*
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+
+        private readonly IHttpClientFactory _clientFactory;
+
+        public HomeController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CircleViewModel model)
+        {
+            var client = _clientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7025/api/huy/Circle/cv_dt?rr={model.rr}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<dynamic>(json);
+                model.DienTich = data.dien_tich;
+                model.ChuVi = data.chu_vi;
+                model.DuongKinh = data.duong_kinh;
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Index()
+        {
+            return View(new CircleViewModel());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
